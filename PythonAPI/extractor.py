0dad0fbcb11a3268d4bccf39112e5b4da83989a7
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-12-16 12:26:29 +0800

    Update extractor.py

--- PythonAPI/extractor.py
+++ PythonAPI/extractor.py
@@ -1,6 +1,7 @@
 from pycocotools.coco import COCO
 import numpy as np
 import skimage.io as io
+from utils.ood_category import load_category
 
 if __name__ == '__main__':
     dataDir = '..'
@@ -20,23 +21,18 @@ if __name__ == '__main__':
     # print('COCO super categories: \n{}'.format(' '.join(nms)))
 
     # get all images containing given categories, select one at random
-    catIds = coco.getCatIds(catNms=['bird'])
-    print('catIDS:', catIds)
-    imgIds = coco.getImgIds(catIds=catIds)
-    # print('imgIDS:', imgIds)
-    # imgIds = coco.getImgIds(imgIds=[222235])
-    print('imgIDS:', imgIds)
-    img = coco.loadImgs(imgIds[np.random.randint(0, len(imgIds))])[0]
+    category = load_category("./utils/category.json")
+    for cat in category:
+        catIds = coco.getCatIds(catNms=[cat])
+        imgIds = coco.getImgIds(catIds=catIds)
+        img = coco.loadImgs(imgIds[np.random.randint(0, len(imgIds))])[0]
 
-    # load and display image
-    image = io.imread('%s/%s/%s' % (dataDir, dataType, img['file_name']))
-    io.imsave('../figs/figs_demo/' + img['file_name'][6:12] + '_original.png', image)
+        image = io.imread('%s/%s/%s' % (dataDir, dataType, img['file_name']))
+        # io.imsave('../figs/figs_demo/' + img['file_name'][6:12] + '_original.png', image)
 
-    # load and display instance annotations
-    annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
-    anns = coco.loadAnns(annIds)
-    mask = coco.polygon_extract(anns, image.shape[0], image.shape[1])
-    for ch in range(3):
-        image[:, :, ch] *= mask
-    
-    io.imsave('../figs/figs_demo/' + img['file_name'][6:12] + '_masked.png', image)
+        annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+        anns = coco.loadAnns(annIds)
+        mask = coco.polygon_extract(anns, image.shape[0], image.shape[1])
+        for ch in range(3):
+            image[:, :, ch] *= mask
+        io.imsave('../figs/figs_demo/' + img['file_name'][6:12] + '_masked.png', image)
