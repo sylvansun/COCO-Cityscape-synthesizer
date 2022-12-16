422263205ad45284d597019b76ca3cbc9b3280b2
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-12-16 12:01:28 +0800

    Update synthesizer.py
    Modified the label of train id, cut the images at default places

--- PythonAPI/synthesizer.py
+++ PythonAPI/synthesizer.py
@@ -9,6 +9,12 @@ def get_city_filename(index):
     name_dict = {"image":imagename, "label":labelname}
     return name_dict
 
+def get_citycut_filename(index,pos):
+    imagename = "zurich_{}_{}_000019_leftImg8bit.png".format(format(index, "06d"),pos)
+    labelname = "zurich_{}_{}_000019_gtFine_labelTrainIds.png".format(format(index, "06d"),pos)
+    name_dict = {"image":imagename, "label":labelname}
+    return name_dict
+
 def load_city_pair(index):
     name_dict = get_city_filename(index)
     image = io.imread("../figs/zurich/image/{}".format(name_dict["image"]))
@@ -19,8 +25,13 @@ def save_city_pair(index, image, label):
     name_dict = get_city_filename(index)
     io.imsave("../figs/zurich/image_syn/{}".format(name_dict["image"]), image)
     io.imsave("../figs/zurich/label_syn/{}".format(name_dict["label"]), label)
+    
+def save_citycut_pair(index, pos, image, label):
+    name_dict = get_citycut_filename(index,pos)
+    io.imsave("../figs/zurich/image_cut/{}".format(name_dict["image"]), image)
+    io.imsave("../figs/zurich/label_cut/{}".format(name_dict["label"]), label)
 
-def main(cats, num_each_cat=10, num_cityscape_img=100, ood_id=20):
+def main(cats, num_each_cat=10, num_cityscape_img=100, ood_id=254, crop_size=480):
     dataDir = '..'
     dataType = 'val2017'
     annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
@@ -46,7 +57,7 @@ def main(cats, num_each_cat=10, num_cityscape_img=100, ood_id=20):
                 mask_city = np.ones((1024,2048)).astype(np.uint8)
                 unmask = np.ones(mask.shape) - mask
                 
-                Hpos,Wpos = np.random.randint(1024-200,1024),np.random.randint(800,2048)
+                Hpos, Wpos = np.random.randint(970,1024), np.random.randint(1000,2048)
                 mask_city[Hpos-H:Hpos,Wpos-W:Wpos] = unmask
                 
                 city,label = load_city_pair(cityscape_count % num_cityscape_img)
@@ -60,8 +71,16 @@ def main(cats, num_each_cat=10, num_cityscape_img=100, ood_id=20):
                 label *= mask_city
                 city[Hpos-H:Hpos,Wpos-W:Wpos,:] += image
                 label[Hpos-H:Hpos,Wpos-W:Wpos] += (ood_id * mask).astype(np.uint8)
-                save_city_pair(cityscape_count, city, label)    
-    
+                save_city_pair(cityscape_count, city, label)
+                cutted_img_labels = []
+                cutted_img_labels.append((city[Hpos-720:Hpos-240,Wpos-720:Wpos-240,:], label[Hpos-720:Hpos-240,Wpos-720:Wpos-240]))
+                cutted_img_labels.append((city[Hpos-2*crop_size:Hpos-crop_size,Wpos-2*crop_size:Wpos-crop_size,:], label[Hpos-2*crop_size:Hpos-crop_size,Wpos-2*crop_size:Wpos-crop_size]))
+                cutted_img_labels.append((city[Hpos-2*crop_size:Hpos-crop_size,Wpos-crop_size:Wpos,:], label[Hpos-2*crop_size:Hpos-crop_size,Wpos-crop_size:Wpos]))
+                cutted_img_labels.append((city[Hpos-crop_size:Hpos,Wpos-2*crop_size:Wpos-crop_size,:], label[Hpos-crop_size:Hpos,Wpos-2*crop_size:Wpos-crop_size]))
+                cutted_img_labels.append((city[Hpos-crop_size:Hpos,Wpos-crop_size:Wpos,:], label[Hpos-crop_size:Hpos,Wpos-crop_size:Wpos]))
+                for i in range(5):
+                    save_citycut_pair(cityscape_count, i + 1, cutted_img_labels[i][0], cutted_img_labels[i][1])
+ 
 if __name__ == "__main__":
     category = load_category("./utils/category.json")
     main(category)
